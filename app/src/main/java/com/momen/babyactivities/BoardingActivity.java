0dbf516d00b773859df0c0a2560317fd8605cede
7aSSan <hassanelkhadrawy59@gmail.com>
2022-04-06 00:37:43 +0200

    into_v1

---
+++ app/src/main/java/com/momen/babyactivities/BoardingActivity.java
@@ -0,0 +1,109 @@
+package com.momen.babyactivities;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardingActivity extends AppCompatActivity {
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicator;
+    private MaterialButton buttonOnboardingAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boarding);
+        layoutOnboardingIndicator = findViewById(R.id.layoutOnboardingIndicators);
+        buttonOnboardingAction = findViewById(R.id.buttonOnBoardingAction);
+        setOnboardingItem();
+        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+        setOnboadingIndicator();
+        setCurrentOnboardingIndicators(0);
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicators(position);
+            }
+        });
+        buttonOnboardingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onboardingViewPager.getCurrentItem() + 1 < onboardingAdapter.getItemCount()) {
+                    onboardingViewPager.setCurrentItem(onboardingViewPager.getCurrentItem() + 1);
+                } else {
+                    startActivity(new Intent(getApplicationContext(), ChooseLevelActivity.class));
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void setOnboadingIndicator() {
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(), R.drawable.indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicator.addView(indicators[i]);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setCurrentOnboardingIndicators(int index) {
+        int childCount = layoutOnboardingIndicator.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutOnboardingIndicator.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.indicator_active));
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.indicator_inactive));
+            }
+        }
+        if (index == onboardingAdapter.getItemCount() - 1) {
+            buttonOnboardingAction.setText("Start");
+        } else {
+            buttonOnboardingAction.setText("Next");
+        }
+    }
+
+    private void setOnboardingItem() {
+        List<OnBoardingItem> onBoardingItems = new ArrayList<>();
+        OnBoardingItem firstBoardingItem = new OnBoardingItem();
+        firstBoardingItem.setImage(R.drawable.img_intro_1);
+        OnBoardingItem secondBoardingItem = new OnBoardingItem();
+        secondBoardingItem.setImage(R.drawable.img_intro_2);
+
+        OnBoardingItem thirdBoardingItem = new OnBoardingItem();
+        thirdBoardingItem.setImage(R.drawable.img_intro_3);
+        OnBoardingItem fourBoardingItem = new OnBoardingItem();
+        fourBoardingItem.setImage(R.drawable.img_intro_4);
+
+        onBoardingItems.add(firstBoardingItem);
+        onBoardingItems.add(secondBoardingItem);
+        onBoardingItems.add(thirdBoardingItem);
+        onBoardingItems.add(fourBoardingItem);
+        onboardingAdapter = new OnboardingAdapter(onBoardingItems);
+    }
+}
